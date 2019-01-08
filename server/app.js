d638a028dd213ab57094e2c6cd395acfe60f1036
zjw <495813672@qq.com>
2019-01-08 15:33:43 +0800

    feat: 添加数据库组件

--- server/app.js
+++ server/app.js
@@ -6,12 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const config = require('./config');
 
 var app = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.listen(config.port);
 
 app.use(logger('dev'));
 app.use(express.json());
---
+++ server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  port: 8080,
+  mysqlHost: 'localhost',
+  mysqlPort: 3306,
+  mysqlName: 'root',
+  mysqlpwd: 'Zhu123456',
+  mysqlDB: 'blog'
+}
